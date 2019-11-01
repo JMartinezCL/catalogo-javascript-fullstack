c9095c6fe4298f6838c4e5d69918fb9790b44186
JMartinezCL <martinez.alvarez.jose77@gmail.com>
2019-11-01 01:47:22 -0300

    add new routes to api rest books

--- backend/routes/books.js
+++ backend/routes/books.js
@@ -1,6 +1,25 @@
 const { Router } = require('express');
 const router = Router();
 
-router.get('/', (req, res) => res.json({text : 'Hello World'}));
+const Book = require('../models/Book');
+
+router.get('/', async (req, res) => {
+    const books = await Book.find();
+    res.json(books);
+});
+
+router.post('/', async (req, res) => {
+    const { title, author, isbn } = req.body;
+    const newBook = new Book( {title, author, isbn} );
+    console.log(newBook);
+    await newBook.save();
+    res.json({message: 'Book Saved'});
+});
+
+router.delete('/:id', async (req, res) => {          
+    const book = await Book.findByIdAndDelete(req.params.id); 
+    console.log(book); 
+    res.json( {message: "Book deleted"} );
+});
 
 module.exports = router;
