9adbcbf51f99bc52a4efcc27ee75c0c650a1c109
JMartinezCL <martinez.alvarez.jose77@gmail.com>
2019-12-05 18:47:57 -0300

    modification on html style and creation readme.md

---
+++ README.md
@@ -0,0 +1,7 @@
+# GETTING STARTED
+exejute the following step
+*npm run dev
+*mongod
+*npm run server:dev
+
+go to http://localhost/8080/ on your browser
--- frontend/UI.js
+++ frontend/UI.js
@@ -17,18 +17,19 @@ class UI{
                     <div class="card-body">
                         <div class="row">
                             <div class="col-md-4">
-                                <img src="http://localhost:3000${book.imagePath}" alt="" class="img-fluid" />
+                                <img src=http://localhost:3000${book.imagePath} alt="" class="img-fluid" />
                             </div>
                             <div class="col-md-8">
                                 <div class="card-block px-2">
-                                <h4 class="card-title"> ${book.title} </h4>
-                                <p class="card-text"> ${book.author} </p>
-                                <a href="#" class="btn btn-danger delete" _id="${book._id}"> x </a>
+                                    <h4 class="card-title"> ${book.title} </h4>
+                                    <p class="card-text"> ${book.author} </p>
+                                    <a href="#" class="btn btn-danger delete" _id="${book._id}"> x </a>
+                                </div>
                             </div>
                         </div>                        
                     </div>
-                    <div class="card-footer text-muted">
-                            ${format(book.created_at)}
+                    <div class="card-footer">
+                        ${format(book.created_at)}
                     </div>
                 </div>
             `;
